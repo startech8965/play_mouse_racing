9edc514f27fe8eab98bb1b5fd3271328b496fdbe
startech8965 <startechincamanda@gmail.com>
2025-11-30 21:21:47 -0500

    Add files via upload

---
+++ GrayWrapper.py
@@ -0,0 +1,15 @@
+import gymnasium as gym
+import numpy as np
+import cv2
+
+class GrayWrapper(gym.ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.observation_space = gym.spaces.Box(
+            low=0, high=255, shape=(84, 84, 1), dtype=np.uint8
+        )
+
+    def observation(self, obs):
+        gray = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
+        resized = cv2.resize(gray, (84, 84), interpolation=cv2.INTER_AREA)
+        return resized[:, :, None]  # Add channel dimension
